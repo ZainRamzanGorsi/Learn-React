56605f7099d0523c17ed6173095d28c9b9d1aac4
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-08-15 09:53:50 +0500

    update the files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import FunctionClick from './components/functionClick';
 import './App.css'
+import FunctionClick from './components/functionClick';
 import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="app">
+      <h1>Learn React JS</h1>
         <FunctionClick /><br></br>
         <ClassClick />
     </div>
